a41134c3b433c3ebd370662d7fef5143bc506690
Julien Soto <juliensoto@free.fr>
2024-02-11 10:40:42 +0100

    Meilleur lisibilté manageRequest dans api.js #2

--- back/queryManagers/api.js
+++ back/queryManagers/api.js
@@ -13,122 +13,135 @@ function setCookie(name, value, daysToLive, response) {
 }
 
 function manageRequest(request, response) {
-  if (request.method === "POST" && request.url === "/api/signin") {
-    let body = "";
-    request.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    request.on("end", async () => {
-      const parsedData = querystring.parse(body);
-
-      try {
-        const db = getDb();
-        const collection = db.collection("users");
-
-        const tokenPayload = {
-          username: parsedData.username,
-          email: parsedData.mail,
-          password: parsedData.password,
-        };
-
-        const existingUser = await collection.findOne({
-          $or: [{ mail: parsedData.mail }, { username: parsedData.username }],
-        });
-
-        if (existingUser) {
-          response.setHeader("Content-Type", "text/html");
-          response.end(
-            `<script>window.location.href = "/connexion.html";alert("Invalid username or password");</script>`
-          );
-          return;
-        }
-        const token = jwt.sign(tokenPayload, parsedData.username);
-
-        const encodedData = {
-          username: parsedData.username,
-          mail: parsedData.mail,
-          token: token,
-        };
-
-        await collection.insertOne(encodedData);
-        response.setHeader("Content-Type", "text/html");
-        response.end(
-          `<script>window.location.href = "/connexion.html";alert("Sign in successful");</script>`
-        );
-      } catch (error) {
-        console.error("Erreur lors de l’insertion des données", error);
-        response.statusCode = 500;
-        response.end(`Erreur serveur`);
-      }
-    });
-  } else if (request.method === "POST" && request.url === "/api/login") {
-    let body = "";
-    request.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    request.on("end", async () => {
-      const parsedData = querystring.parse(body);
-
-      try {
-        const db = getDb();
-        const collection = db.collection("users");
-
-        const existingUser = await collection.findOne({
-          $or: [{ mail: parsedData.login }, { username: parsedData.login }],
-        });
-
-        if (!existingUser) {
-          response.setHeader("Content-Type", "text/html");
-          response.end(
-            `<script>window.location.href = "/connexion.html";alert("Wrong username");</script>`
-          );
-          return;
-        }
-
-        const tokenPayload = {
-          email: existingUser.mail,
-          password: parsedData.password,
-        };
-        const decodedToken = jwt.verify(
-          existingUser.token,
-          existingUser.username
-        );
-
-        if (tokenPayload.password != decodedToken.password) {
-          response.setHeader("Content-Type", "text/html");
-          response.end(
-            `<script>window.location.href = "/connexion.html";alert("Wrong username or password");</script>`
-          );
-          return;
-        }
-        setCookie("connected", existingUser.token, 1, response);
-        response.setHeader("Content-Type", "text/html");
-        response.end(
-          `<script>window.location.href = "/connexion.html";alert("Connexion success");</script>`
-        );
-      } catch (error) {
-        console.error("Erreur lors de la recherche de l'utilisateur", error);
-        response.statusCode = 500;
-        response.end(`Erreur serveur`);
-      }
-    });
-  } else if (request.method === "POST" && request.url === "/api/logout") {
-    try {
-      setCookie("connected", "", -1, response);
-      response.setHeader("Content-Type", "text/html");
-      response.end(
-        `<script>window.location.href = "/connexion.html";alert("You have been logged out successfully.");</script>`
-      );
-    } catch (error) {
-      console.error("Erreur lors de la déconnexion", error);
-      response.statusCode = 500;
-      response.end("Erreur serveur lors de la déconnexion");
+  if (request.method === "POST") {
+    switch (request.url) {
+      case "/api/signin":
+        handleSignIn(request, response);
+        break;
+      case "/api/login":
+        handleLogin(request, response);
+        break;
+      case "/api/logout":
+        handleLogout(request, response);
+        break;
+      default:
+        response.end(`Merci d'avoir appelé ${request.url}`);
     }
-  } else {
-    response.end(`Merci d'avoir appelé ${request.url}`);
   }
 }
 
+async function handleSignIn(request, response) {
+  let body = "";
+  request.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  request.on("end", async () => {
+    const parsedData = querystring.parse(body);
+
+    try {
+      const db = getDb();
+      const collection = db.collection("users");
+
+      const tokenPayload = {
+        username: parsedData.username,
+        email: parsedData.mail,
+        password: parsedData.password,
+      };
+
+      const existingUser = await collection.findOne({
+        $or: [{ mail: parsedData.mail }, { username: parsedData.username }],
+      });
+
+      if (existingUser) {
+        response.setHeader("Content-Type", "text/html");
+        response.end(
+          `<script>window.location.href = "/connexion.html";alert("Invalid username or password");</script>`
+        );
+        return;
+      }
+      const token = jwt.sign(tokenPayload, parsedData.username);
+
+      const encodedData = {
+        username: parsedData.username,
+        mail: parsedData.mail,
+        token: token,
+      };
+
+      await collection.insertOne(encodedData);
+      response.setHeader("Content-Type", "text/html");
+      response.end(
+        `<script>window.location.href = "/connexion.html";alert("Sign in successful");</script>`
+      );
+    } catch (error) {
+      console.error("Erreur lors de l’insertion des données", error);
+      response.statusCode = 500;
+      response.end(`Erreur serveur`);
+    }
+  });
+}
+
+async function handleLogin(request, response) {
+  let body = "";
+  request.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  request.on("end", async () => {
+    const parsedData = querystring.parse(body);
+
+    try {
+      const db = getDb();
+      const collection = db.collection("users");
+
+      const existingUser = await collection.findOne({
+        $or: [{ mail: parsedData.login }, { username: parsedData.login }],
+      });
+
+      if (!existingUser) {
+        response.setHeader("Content-Type", "text/html");
+        response.end(
+          `<script>window.location.href = "/connexion.html";alert("Wrong username");</script>`
+        );
+        return;
+      }
+
+      const decodedToken = jwt.verify(
+        existingUser.token,
+        existingUser.username
+      );
+
+      if (parsedData.password != decodedToken.password) {
+        response.setHeader("Content-Type", "text/html");
+        response.end(
+          `<script>window.location.href = "/connexion.html";alert("Wrong username or password");</script>`
+        );
+        return;
+      }
+      setCookie("connected", existingUser.token, 1, response);
+      response.setHeader("Content-Type", "text/html");
+      response.end(
+        `<script>window.location.href = "/connexion.html";alert("Connexion success");</script>`
+      );
+    } catch (error) {
+      console.error("Erreur lors de la recherche de l'utilisateur", error);
+      response.statusCode = 500;
+      response.end(`Erreur serveur`);
+    }
+  });
+}
+
+function handleLogout(request, response) {
+  try {
+    setCookie("connected", "", -1, response);
+    response.setHeader("Content-Type", "text/html");
+    response.end(
+      `<script>window.location.href = "/connexion.html";alert("You have been logged out successfully.");</script>`
+    );
+  } catch (error) {
+    console.error("Erreur lors de la déconnexion", error);
+    response.statusCode = 500;
+    response.end("Erreur serveur lors de la déconnexion");
+  }
+}
 /* This method is a helper in case you stumble upon CORS problems. It shouldn't be used as-is:
  ** Access-Control-Allow-Methods should only contain the authorized method for the url that has been targeted
  ** (for instance, some of your api urls may accept GET and POST request whereas some others will only accept PUT).
