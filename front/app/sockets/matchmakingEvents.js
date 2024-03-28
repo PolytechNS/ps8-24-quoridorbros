de0835f76a5339c9dd3c5fcc2eec11436e859f01
DorianBD <bouchard.dorian@gmail.com>
2024-03-28 19:31:30 +0100

    feat: récupération des profils Ref #25

--- front/app/sockets/matchmakingEvents.js
+++ front/app/sockets/matchmakingEvents.js
@@ -1,4 +1,7 @@
 socket.on("RoomFull", (msg) => {
+  socket.emit("Acknowledgement", msg.id);
   console.log("RoomFull");
+  let profileOpponentDataString = JSON.stringify(msg.data);
+  localStorage.setItem("profileOpponentString", profileOpponentDataString);
   window.location.href = "../onlineGame/onlineGame.html";
 });
