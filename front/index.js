676f50215bb39c28aa112fe6beee73d08bc6d9e3
EvanTranvouez <evan.tranvouez@etu.unice.fr>
2024-04-06 20:12:41 +0200

    #38 Disable online play when unlogged

--- front/index.js
+++ front/index.js
@@ -2,6 +2,8 @@ let playWithAIButton = document.getElementById("playAIButton");
 let playLocalButton = document.getElementById("playLocalButton");
 let playOnlineButton = document.getElementById("playOnlineButton");
 let logoutButton = document.getElementById("logoutButton");
+let playButton = document.getElementById("playButton");
+let backButton = document.getElementById("backButton");
 
 window.onload = function () {
   let connectedCookieValue = getCookie("connected");
@@ -10,9 +12,15 @@ window.onload = function () {
     document.getElementById("logoutButton").style.display = "block";
     document.getElementById("loginButton").style.display = "none";
     document.getElementById("signinButton").style.display = "none";
-    playWithAIButton.style.display = "inline";
-    playLocalButton.style.display = "inline";
-    playOnlineButton.style.display = "inline";
+    playWithAIButton.style.display = "none";
+    playLocalButton.style.display = "none";
+    playOnlineButton.style.display = "none";
+    playOnlineButton.disabled = false;
+    document.getElementById("loginNote").style.display = "none";
+    playOnlineButton.classList.add("mainButtonClass");
+    playOnlineButton.classList.remove("mainButtonDisabledClass");
+    playButton.style.display = "inline";
+    backButton.style.display = "none";
   }
 };
 
@@ -29,9 +37,24 @@ document.getElementById("logoutButton").addEventListener("click", function () {
         playWithAIButton.style.display = "none";
         playLocalButton.style.display = "none";
         playOnlineButton.style.display = "none";
+        playOnlineButton.disabled = true;
+        document.getElementById("loginNote").style.display = "inline";
+        playOnlineButton.classList.remove("mainButtonClass");
+        playOnlineButton.classList.add("mainButtonDisabledClass");
+        playButton.style.display = "inline";
+        backButton.style.display = "none";
         alert("Deconnexion Successful");
       }
     })
     .catch((error) => console.error("Error:", error));
 });
 
+document.getElementById("playButton").addEventListener("click", function () {
+  playWithAIButton.style.display = "inline";
+  playLocalButton.style.display = "inline";
+  playOnlineButton.style.display = "inline";
+  playButton.style.display = "none";
+  backButton.style.display = "inline";
+  document.getElementById("loginButton").style.display = "none";
+  document.getElementById("signinButton").style.display = "none";
+});
