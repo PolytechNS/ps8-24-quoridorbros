deb8347c866c32c4806892bca6129f86371bf375
DorianBD <bouchard.dorian@gmail.com>
2024-04-24 00:02:16 +0200

    fix:  challenges friends fonctionnel

--- front/app/challengeModal/challengeModal.js
+++ front/app/challengeModal/challengeModal.js
@@ -2,64 +2,68 @@ const challengeModalContainer = document.getElementById(
   "challenge-modal-container",
 );
 
-
 function setUpChallengeModalClosingListeners() {
   document
     .getElementsByClassName("close")[0]
     .addEventListener("click", function () {
-      challengeModalContainer.innerHTML = "";
+      const usernamediv = document.getElementById("usernamechallenge");
+      declineChallenge(usernamediv.textContent);
     });
 
   window.addEventListener("click", function (event) {
     if (event.target == document.getElementById("challengeModal")) {
-      challengeModalContainer.innerHTML = "";
+      const usernamediv = document.getElementById("usernamechallenge");
+      declineChallenge(usernamediv.textContent);
     }
   });
 }
-
 
 function acceptChallenge(username) {
   socket.emit("acceptChallenge", username);
 }
 
 function declineChallenge(username) {
-    socket.emit("declineChallenge", username);
+  challengeModalContainer.innerHTML = "";
+  socket.emit("declineChallenge", username);
 }
 
 function displayChallenger(profileOpponent) {
-    const challenger = profileOpponent;
+  const challenger = profileOpponent;
 
-    const challengerElement = document.createElement("div");
-    challengerElement.classList.add("challenger");
+  const challengerElement = document.createElement("div");
+  challengerElement.classList.add("challenger");
 
-    const profilePic = document.createElement("img");
-    profilePic.src = challenger.photo;
-    profilePic.classList.add("profile-picture");
-    challengerElement.appendChild(profilePic);
+  const profilePic = document.createElement("img");
+  profilePic.src = challenger.photo;
+  profilePic.classList.add("profile-picture");
+  challengerElement.appendChild(profilePic);
 
-    const usernameElement = document.createElement("div");
-    usernameElement.textContent = challenger.username;
-    usernameElement.classList.add("username");
-    challengerElement.appendChild(usernameElement);
+  const usernameElement = document.createElement("div");
+  usernameElement.textContent = challenger.username;
+  usernameElement.id = "usernamechallenge";
+  usernameElement.classList.add("username");
+  challengerElement.appendChild(usernameElement);
 
-    const acceptButton = document.createElement("button");
-    acceptButton.textContent = "Accept";
-    acceptButton.classList.add("mainButtonClass");
-    acceptButton.addEventListener("click", () => {
-        acceptChallenge(challenger.username);
-    });
-    challengerElement.appendChild(acceptButton);
+  const acceptButton = document.createElement("button");
+  acceptButton.textContent = "Accept";
+  acceptButton.classList.add("mainButtonClass");
+  acceptButton.addEventListener("click", () => {
+    acceptChallenge(challenger.username);
+  });
+  challengerElement.appendChild(acceptButton);
 
-    const declineButton = document.createElement("button");
-    declineButton.textContent = "Decline";
-    declineButton.classList.add("mainButtonClass");
-    declineButton.addEventListener("click", () => {
-        declineChallenge(challenger.username);
-    });
-    challengerElement.appendChild(declineButton);
+  const declineButton = document.createElement("button");
+  declineButton.textContent = "Decline";
+  declineButton.classList.add("mainButtonClass");
+  declineButton.addEventListener("click", () => {
+    declineChallenge(challenger.username);
+  });
+  challengerElement.appendChild(declineButton);
 
-    document.getElementById("challenger-container").appendChild(challengerElement);
-    }
+  document
+    .getElementById("challenger-container")
+    .appendChild(challengerElement);
+}
 
 async function loadChallengeModal(profileOpponent) {
   const response = await fetch("../../app/challengeModal/challengeModal.html");
@@ -69,6 +73,3 @@ async function loadChallengeModal(profileOpponent) {
   displayChallenger(profileOpponent);
   document.getElementById("challengeModal").style.display = "block";
 }
-
-
-
